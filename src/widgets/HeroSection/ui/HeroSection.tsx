8d8149e89baf1f4cd0f6ba4b71079a5c602380a5
Nikita Bakumenko <n.bakumenko@numedy.com>
2024-09-13 17:54:40 +0300

    style: fix styles for hero and question sections to pixel perfect

--- src/widgets/HeroSection/ui/HeroSection.tsx
+++ src/widgets/HeroSection/ui/HeroSection.tsx
@@ -6,11 +6,8 @@ export const HeroSection = () => {
   return (
     <Container wrapperClassName={s.hero}>
       <div className={s.heroContent}>
-        <p className={s.heroBigText}>Any products from famous brands
-          with worldwide delivery</p>
-        <p className={s.heroSmallText}>We sell smartphones, laptops, clothes, shoes
-          {/*<br/>*/}
-          and many other products at low prices</p>
+        <p className={s.heroBigText}>Any products from famous brands with worldwide delivery</p>
+        <p className={s.heroSmallText}>We sell smartphones, laptops, clothes, shoes and many other products at low prices</p>
         <ButtonWithChild className={s.heroButton}>
           Go to shopping
         </ButtonWithChild>
