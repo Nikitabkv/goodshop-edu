d4cccc9b0ba3736edfe5ee89f7fea61ada8fe632
Nikita Bakumenko <n.bakumenko@numedy.com>
2024-09-13 16:20:30 +0300

    style: add adaptive for main page

--- src/widgets/HeroSection/ui/HeroSection.tsx
+++ src/widgets/HeroSection/ui/HeroSection.tsx
@@ -6,8 +6,11 @@ export const HeroSection = () => {
   return (
     <Container wrapperClassName={s.hero}>
       <div className={s.heroContent}>
-        <p className={s.heroBigText}>Any products from famous brands<br/>with worldwide delivery</p>
-        <p className={s.heroSmallText}>We sell smartphones, laptops, clothes, shoes<br/>and many other products at low prices</p>
+        <p className={s.heroBigText}>Any products from famous brands
+          with worldwide delivery</p>
+        <p className={s.heroSmallText}>We sell smartphones, laptops, clothes, shoes
+          {/*<br/>*/}
+          and many other products at low prices</p>
         <ButtonWithChild className={s.heroButton}>
           Go to shopping
         </ButtonWithChild>
