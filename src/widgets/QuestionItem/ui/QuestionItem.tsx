6ee0605fe0ec726985e1299511b178e7feea3755
n.bakumenko <n.bakumenko@numedy.com>
2024-09-15 17:30:34 +0300

    feat: add isOpen prop for QuestionItem

--- src/widgets/QuestionItem/ui/QuestionItem.tsx
+++ src/widgets/QuestionItem/ui/QuestionItem.tsx
@@ -5,20 +5,21 @@ import {CrossIcon} from "../../../shared/icons"
 interface QuestionProps {
   question: string
   answer: string
+  isOpen?: boolean
 }
 
-export const QuestionItem:FC<QuestionProps> = ({question, answer}) => {
-  const [isOpen, setIsOpen] = useState(false)
+export const QuestionItem:FC<QuestionProps> = ({question, answer, isOpen = false}) => {
+  const [questionIsOpen, setQuestionIsOpen] = useState(isOpen)
 
   return (
-    <div className={s.wrapper + (isOpen ? ' ' + s.open : '')}>
-      <div className={s.questionLine} onClick={() => setIsOpen(!isOpen)}>
+    <div className={s.wrapper + (questionIsOpen ? ' ' + s.open : '')}>
+      <div className={s.questionLine} onClick={() => setQuestionIsOpen(!questionIsOpen)}>
         <span className={s.question}>{question}</span>
-        <span className={s.crossIconWrapper + (isOpen ? ' ' + s.open : '')}>
+        <span className={s.crossIconWrapper + (questionIsOpen ? ' ' + s.open : '')}>
           <CrossIcon />
         </span>
       </div>
-      <div className={s.answerLine + (isOpen ? ' ' + s.open : '')}>
+      <div className={s.answerLine + (questionIsOpen ? ' ' + s.open : '')}>
         {answer}
       </div>
     </div>
--- src/widgets/QuestionsSection/ui/QuestionsSection.tsx
+++ src/widgets/QuestionsSection/ui/QuestionsSection.tsx
@@ -10,7 +10,7 @@ export const QuestionsSection = () => {
       <Title tag={'h2'} className={s.title}>
         FAQ
       </Title>
-      <QuestionItem question={'How can I track the status of my order?'} answer={'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'}/>
+      <QuestionItem isOpen question={'How can I track the status of my order?'} answer={'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'}/>
       <QuestionItem question={'What payment methods do you accept?'} answer={'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'}/>
       <QuestionItem question={'How can I return or exchange an item?'} answer={'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'}/>
     </Container>
