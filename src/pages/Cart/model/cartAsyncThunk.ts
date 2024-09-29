65665ef42fa997d816d8929e72b2feeb60bb9fa9
n.bakumenko <n.bakumenko@numedy.com>
2024-09-29 22:59:12 +0300

    fix: todo types

--- src/pages/Cart/model/cartAsyncThunk.ts
+++ src/pages/Cart/model/cartAsyncThunk.ts
@@ -1,6 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
+interface Product {
+  id: number
+  quantity: number
+}
+
 export const getCartByUser = createAsyncThunk(
   'cart/getCartByUser',
   async (userId: number, thunkAPI) => {
@@ -19,8 +24,8 @@ export const getCartByUser = createAsyncThunk(
 
 export const updateProduct = createAsyncThunk(
   'cart/updateProduct',
-  //TODO: any
-  async ({products, cartId}: {products: any, cartId: number}, thunkAPI) => {
+  async ({products, cartId}: {products: Array<Product>, cartId: number}, thunkAPI) => {
+    console.log(products)
     try {
       const response = await axios.put(`https://dummyjson.com/auth/carts/${cartId}`, {
         merge: false,
